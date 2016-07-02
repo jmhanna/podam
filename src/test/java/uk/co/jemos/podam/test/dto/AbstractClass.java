e256e415db08940de0df3b3fbdf01ec6548cbae4
Joe <joe.mhanna@gmail.com>
2016-07-02 18:57:53 +0300

    test by joseph mhanna

--- src/test/java/uk/co/jemos/podam/test/dto/AbstractClass.java
+++ src/test/java/uk/co/jemos/podam/test/dto/AbstractClass.java
@@ -3,6 +3,7 @@ package uk.co.jemos.podam.test.dto;
 /**
  * Class which doesn't have any specific implementation
  * @author daivanov
+ * This is a test by joseph mhanna
  */
 public abstract class AbstractClass {
 	private String name;
